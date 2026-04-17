d9a64400a019fe89c8c316dc1e86b9ec2964c0a6
LillianCelie <leeyuri998@gmail.com>
2026-04-17 17:34:59 +0800

    config

--- config.php
+++ config.php
@@ -1,14 +1,13 @@
 <?php
-
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $password = "";
 $database = "users_db";
+$port = 3307;
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
