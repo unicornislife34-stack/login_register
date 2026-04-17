ed4790d3424e7b456f6048e99c085496a236cf37
LillianCelie <leanmichellel@gmail.com>
2026-04-17 15:53:13 +0800

    Delete revise ver 4 directory

--- revise ver 4/check_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-$result = $conn->query('SHOW TABLES');
-while($row = $result->fetch_array()) {
-    echo $row[0] . PHP_EOL;
-}
-?>
--- revise ver 4/script.js
+++
@@ -1,6 +0,0 @@
-// Function to show the selected form and hide the others
-function showForm(formId) {
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
-
